87a990c7f42b54423c51747471dc7d2b0f0c32b7
Patrick Old <patold@D-172-28-102-57.dhcp4.washington.edu>
2018-12-07 15:28:24 -0800

    WOEID code working

--- Trendsy/Trendsy/Constants.swift
+++ Trendsy/Trendsy/Constants.swift
@@ -4,10 +4,14 @@
 //
 //  Created by Patrick Old on 12/6/18.
 //  Copyright © 2018 Michelle Ho. All rights reserved.
-//
+
+
 import Foundation
 
 let apiKey = "IfftqnrrPXzFVReJyOYWpCkQ9"
 let apiSecret = "RD16NwddbSbHD1ybCqx9ObwDxlCbLK4z6lTICa2YOG9LXH5QP5"
 let accessToken = "1068641422201372673-jD0EY4vEBdJYsoQcYD7C1qP3imKQgd"
 let accessTokenSecret = "Frnwy3I7ewnvpS6LCzV07J4vJ7oCtlNAtwJGgYb1F28th"
+
+let YaConsumerKey = "dj0yJmk9Rm1Wa1hCUU0yWmVPJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWZl"
+let YaClientSecret = "4fbb38a0509945d6bb69988cfb5b9e4125f33474"
--- Trendsy/Trendsy/FirstViewController.swift
+++ Trendsy/Trendsy/FirstViewController.swift
@@ -11,11 +11,39 @@ import OhhAuth
 class FirstViewController: UIViewController {
     
     override func viewDidLoad() {
+        
+        // YAHOO WOEID API CODE
+        let cc = (key: YaConsumerKey, secret: YaClientSecret)
+        let location = "Seattle"
+        let test = "https://query.yahooapis.com/v1/public/yql?q=select+*+from+geo.places+where+text%3D%22" + location + "%22&format=json"
+        var req = URLRequest(url: URL(string: test)!)
+        
+        req.oAuthSign(method: "GET", consumerCredentials: cc)
+        
+        
+        let task = URLSession(configuration: .ephemeral).dataTask(with: req) { (data, response, error) in
+            
+            if let error = error {
+                print(error)
+            }
+            else if let data = data {
+                print(String(data: data, encoding: .utf8) ?? "Does not look like a utf8 response :(")
+                
+            }
+        }
+        task.resume()
+        
+        
+        // TWITTER API CODE
+        
+        /*
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let cc = (key: apiKey, secret: apiSecret)
         let uc = (key: accessToken, secret: accessTokenSecret)
         
+         
+        // REPLACE id=1 with the desired WOEID from YAHOO API
         var req = URLRequest(url: URL(string: "https://api.twitter.com/1.1/trends/place.json?id=1")!)
         
         req.oAuthSign(method: "GET", consumerCredentials: cc, userCredentials: uc)
@@ -30,6 +58,7 @@ class FirstViewController: UIViewController {
             }
         }
         task.resume()
+        */
+        
     }
-    
 }
