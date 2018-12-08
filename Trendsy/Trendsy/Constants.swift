4dec8c5b68ab5dbe6ec28cd8c6c9fcbecca8f467
Michelle <michho@uw.edu>
2018-12-08 14:28:01 -0800

    added gitignore

---
+++ Trendsy/Trendsy/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Trendsy
+//
+//  Created by Michelle Ho on 12/8/18.
+//  Copyright © 2018 Michelle Ho. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "IfftqnrrPXzFVReJyOYWpCkQ9"
+let apiSecret = "RD16NwddbSbHD1ybCqx9ObwDxlCbLK4z6lTICa2YOG9LXH5QP5"
+let accessToken = "1068641422201372673-jD0EY4vEBdJYsoQcYD7C1qP3imKQgd"
+let accessTokenSecret = "Frnwy3I7ewnvpS6LCzV07J4vJ7oCtlNAtwJGgYb1F28th"
