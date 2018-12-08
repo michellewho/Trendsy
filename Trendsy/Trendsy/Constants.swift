2610cd72504f8a692cf2b45e492c0a386e97ebf0
Michelle <michho@uw.edu>
2018-12-08 14:55:30 -0800

    edited Constatnts

--- Trendsy/Trendsy/Constants.swift
+++ Trendsy/Trendsy/Constants.swift
@@ -12,3 +12,4 @@ let apiKey = "IfftqnrrPXzFVReJyOYWpCkQ9"
 let apiSecret = "RD16NwddbSbHD1ybCqx9ObwDxlCbLK4z6lTICa2YOG9LXH5QP5"
 let accessToken = "1068641422201372673-jD0EY4vEBdJYsoQcYD7C1qP3imKQgd"
 let accessTokenSecret = "Frnwy3I7ewnvpS6LCzV07J4vJ7oCtlNAtwJGgYb1F28th"
+
