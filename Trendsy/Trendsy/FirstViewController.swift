dce9bd4851671cfbbdba7c11970fa0f4b9a2fd28
Patrick Old <patold@Patricks-MacBook-Pro.local>
2018-12-08 17:58:28 -0800

    testing to merge master

--- Trendsy/Trendsy/FirstViewController.swift
+++ Trendsy/Trendsy/FirstViewController.swift
@@ -13,7 +13,8 @@ class FirstViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //dispatchFunc()
+        var location = "Argentina"
+        //dispatchFunc(location)
         //print("TEST")
         getTweetsWithHashtag(searchTopic: "SeduceMeIn4Words")
     }
@@ -34,13 +35,47 @@ class FirstViewController: UIViewController {
                 print(error)
             }
             else if let data = data {
+                var test = String(data: data, encoding: .utf8) ?? "Does not look like a utf8 response :("
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                    print ("JSON", json)
+                    for item in json! {
+                        if let inner = item.value as? [String: Any] {
+                            print ("got in here hello")
+                            print ("inner", inner)
+                            /*
+                            if let results = inner["results"] as? [String : Any] {
+                                if let places = results["place"] as? NSArray {
+                                    let place = places[0] as! NSDictionary
+                                    var woeid = Int((place["woeid"] as! NSString) as String)!
+                                    currWOEID = woeid
+                                }
+                            }
+                            */
+                        }
+                    }
+                } catch {
+                    print("Error deserializing JSON: \(error)")
+                }
+            }
+        }
+        task.resume()
+        /*
+        
+        let task = URLSession(configuration: .ephemeral).dataTask(with: req) { (data, response, error)
+            in
+            
+            if let error = error {
+                print(error)
+            }
+            else if let data = data {
                 var newData = String(data: data, encoding: .utf8) ?? "Does not look like a utf8 response :("
                 print(newData)
                 print(String(data: data, encoding: .utf8) ?? "Does not look like a utf8 response :(")
                 
-                //let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
-                //print (json)
-                /*
+                let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                print(json)
+
                  for item in json! {
                  if let inner = item.value as? [String: Any] {
                  if let results = inner["results"] as? [String : Any] {
@@ -52,19 +87,19 @@ class FirstViewController: UIViewController {
                  }
                  }
                  }
-                 */
+         
             }
         }
+*/
         
         task.resume()
     }
     
-    func dispatchFunc() {
+    func dispatchFunc(location: String) {
         let group = DispatchGroup()
-        var location = "Argentina"
         var currWOEID = -1
         // change spaces so they can work in URL
-        location = location.replacingOccurrences(of: " ", with: "%20", options: .literal, range: nil)
+        var locationChanged = location.replacingOccurrences(of: " ", with: "%20", options: .literal, range: nil)
         //-----------------------------
         group.enter()
         //------------------------------
@@ -72,7 +107,7 @@ class FirstViewController: UIViewController {
         func woeID() {
             // YAHOO WOEID API CODE
             let cc = (key: YaConsumerKey, secret: YaClientSecret)
-            let test = "https://query.yahooapis.com/v1/public/yql?q=select+*+from+geo.places+where+text%3D%22" + location + "%22&format=json"
+            let test = "https://query.yahooapis.com/v1/public/yql?q=select+*+from+geo.places+where+text%3D%22" + locationChanged + "%22&format=json"
             var req = URLRequest(url: URL(string: test)!)
             req.oAuthSign(method: "GET", consumerCredentials: cc)
             
