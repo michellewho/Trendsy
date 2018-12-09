e4a2ca92c3e3a31d02742547d921ce583b29a2aa
Michelle <michho@uw.edu>
2018-12-09 15:43:39 -0800

    renamed FirstViewController and edited UI

--- Trendsy/Trendsy/JSONController.swift
+++ Trendsy/Trendsy/JSONController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  JSONController.swift
 //  Trendsy
 //
 //  Created by Michelle Ho on 11/26/18.
@@ -10,12 +10,7 @@ import UIKit
 import OhhAuth
 
 
-class FirstViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    
-    var cellColor = true
-    
-    let reuseIdentifier = "collectionReuseIdentifier"
-    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+class JSONController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -25,40 +20,6 @@ class FirstViewController: UIViewController, UICollectionViewDataSource, UIColle
         
         
     }
-    
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        // get a reference to our storyboard cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CustomCollectionViewCell
-        
-//        cell.label.text = self.items[indexPath.item]
-        cell.backgroundColor = UIColor.cyan
-        cell.layer.borderColor = UIColor.black.cgColor
-        cell.layer.borderWidth = 1
-        cell.layer.cornerRadius = 8
-        
-        //        print(collectionView.frame.size.width)
-        //        print(collectionView.frame.size.height)
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // handle tap events
-        print("You selected cell #\(indexPath.item)!")
-    }
-    
     
     func getTweetsWithHashtag(searchTopic: String) {
         let cc = (key: apiKey, secret: apiSecret)
